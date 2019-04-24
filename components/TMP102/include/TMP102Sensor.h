7101897af605cd18e2133226ae5fc9a97737a808
Carbon <carbon.szlachta@gmail.com>
2019-04-24 22:41:39 +0200

    Added TMP settings

--- components/TMP102/include/TMP102Sensor.h
+++ components/TMP102/include/TMP102Sensor.h
@@ -33,13 +33,15 @@ protected:
 						// Sensor::sendToQueues(ErrorTypeToElement(ErrorTypes::I2CBlocked));
 					}
 					count++;
-					vTaskDelay(5 / portTICK_PERIOD_MS);
+					vTaskDelay(10 / portTICK_PERIOD_MS);
 				}
 
 				// read sensor and give back semaphore
-				// TMP102::wakeup();
+				TMP102::wakeup();
+				vTaskDelay(10 / portTICK_PERIOD_MS);
 				float temp = TMP102::readTempC();
-				// TMP102::sleep();
+				vTaskDelay(10 / portTICK_PERIOD_MS);
+				TMP102::sleep();
 
 				xSemaphoreGive(i2c_mutex);
 
@@ -87,6 +89,36 @@ public:
 		
 			TMP102::begin();
 			vTaskDelay(10 / portTICK_PERIOD_MS);
+			
+			// Initialize sensor0 settings
+			// These settings are saved in the sensor, even if it loses power
+
+			// set the number of consecutive faults before triggering alarm.
+			// 0-3: 0:1 fault, 1:2 faults, 2:4 faults, 3:6 faults.
+			TMP102::setFault(3);  // Trigger alarm immediately
+
+			// set the polarity of the Alarm. (0:Active LOW, 1:Active HIGH).
+			TMP102::setAlertPolarity(1); // Active HIGH
+
+			// set the sensor in Comparator Mode (0) or Interrupt Mode (1).
+			TMP102::setAlertMode(0); // Comparator Mode.
+
+			// set the Conversion Rate (how quickly the sensor gets a new reading)
+			//0-3: 0:0.25Hz, 1:1Hz, 2:4Hz, 3:8Hz
+			TMP102::setConversionRate(3);
+
+			//set Extended Mode.
+			//0:12-bit Temperature(-55C to +128C) 1:13-bit Temperature(-55C to +150C)
+			TMP102::setExtendedMode(0);
+
+			//set T_HIGH, the upper limit to trigger the alert on
+			// sensor0.setHighTempF(85.0);  // set T_HIGH in F
+			//sensor0.setHighTempC(29.4); // set T_HIGH in C
+
+			//set T_LOW, the lower limit to shut turn off the alert
+			// sensor0.setLowTempF(84.0);  // set T_LOW in F
+			//sensor0.setLowTempC(26.67); // set T_LOW in C
+
 			TMP102::wakeup();
 
 			xSemaphoreGive(i2c_mutex);
